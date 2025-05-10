29d48683aabd682278a99ab1be4f05811e85f3ce
ABOUBAKERABOUELKACIM <ABOUBAKERABOUELKACIM123@gmail.com>
2025-05-10 11:00:50 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/Sehawave_aunch/',
+   base: '/Sehawave_aunch',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
